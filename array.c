f685e7f9bef3d3c8f9a449fb72de3c9cbcdf8fbf
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-05-10 17:56:44 +0530

    Added array_void.h file and a typedef of array_ptr in array.h

--- array.c
+++ array.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include "array.h"
 
-Array *create_array(int length)
+Array_ptr create_array(int length)
 {
-  Array *array = (Array *)malloc(sizeof(Array));
+  Array_ptr array = (Array_ptr)malloc(sizeof(Array));
   array->length = length;
   array->array = (int *)malloc(sizeof(int) * length);
   return array;
 }
 
-Array *create_and_initialize_array(int *src, int length)
+Array_ptr create_and_initialize_array(int *src, int length)
 {
-  Array *array = create_array(length);
+  Array_ptr array = create_array(length);
   for (int index = 0; index < length; index++)
   {
     array->array[index] = src[index];
@@ -20,9 +20,9 @@ Array *create_and_initialize_array(int *src, int length)
   return array;
 }
 
-Array *map(Array *src, Mapper mapper)
+Array_ptr map(Array_ptr src, Mapper mapper)
 {
-  Array *array = create_array(src->length);
+  Array_ptr array = create_array(src->length);
   for (int index = 0; index < src->length; index++)
   {
     array->array[index] = mapper(src->array[index]);
@@ -30,7 +30,7 @@ Array *map(Array *src, Mapper mapper)
   return array;
 }
 
-Array *filter(Array *src, Predicate predicate)
+Array_ptr filter(Array_ptr src, Predicate predicate)
 {
   int temp[src->length], count = 0;
   for (int index = 0; index < src->length; index++)
@@ -45,7 +45,7 @@ Array *filter(Array *src, Predicate predicate)
   return create_and_initialize_array(temp, count);
 }
 
-int reduce(Array *src, int init, Reducer reducer)
+int reduce(Array_ptr src, int init, Reducer reducer)
 {
   int context = init;
   for (int index = 0; index < src->length; index++)
--- array.h
+++ array.h
@@ -16,11 +16,13 @@ typedef struct
   int length;
 } Array;
 
-Array *create_array(int length);
-Array *create_and_initialize_array(int *array, int length);
+typedef Array *Array_ptr;
 
-Array *map(Array *src, Mapper mapper);
-Array *filter(Array *src, Predicate predicate);
-int reduce(Array *src, int init, Reducer reducer);
+Array_ptr create_array(int length);
+Array_ptr create_and_initialize_array(int *array, int length);
+
+Array_ptr map(Array_ptr src, Mapper mapper);
+Array_ptr filter(Array_ptr src, Predicate predicate);
+int reduce(Array_ptr src, int init, Reducer reducer);
 
 #endif
