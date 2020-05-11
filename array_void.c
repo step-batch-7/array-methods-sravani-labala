6e33337c8ed403671753c7292c7f3d942ba0f3bc
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-05-11 13:26:58 +0530

    Fixed a bug
    Using heap memory for the storing of values in the array of array_void pointer instead of stack memory
    Removed create_and_initialize_array_void function in array_void files

--- array_void.c
+++ array_void.c
@@ -15,19 +15,9 @@ ArrayVoid_ptr map_void(ArrayVoid_ptr src, MapperVoid mapper)
   ArrayVoid_ptr array = create_array_void(src->length);
   for (int index = 0; index < src->length; index++)
   {
-    array->array[index] = mapper(src->array[index]);
+    array->array[index] = (*mapper)(src->array[index]);
   }
   return array;
-}
-
-ArrayVoid_ptr create_and_initialize_void_array(Object *src, int length)
-{
-  ArrayVoid_ptr array_void = create_array_void(length);
-  for (int index = 0; index < length; index++)
-  {
-    array_void->array[index] = src[index];
-  }
-  return array_void;
 }
 
 ArrayVoid_ptr filter_void(ArrayVoid_ptr src, PredicateVoid predicate)
@@ -36,14 +26,19 @@ ArrayVoid_ptr filter_void(ArrayVoid_ptr src, PredicateVoid predicate)
   int count = 0;
   for (int index = 0; index < src->length; index++)
   {
-    Bool status = predicate(src->array[index]);
+    Bool status = (*predicate)(src->array[index]);
     if (status)
     {
       temp[count] = src->array[index];
       count++;
     }
   }
-  return create_and_initialize_void_array(temp, count);
+  ArrayVoid_ptr array_void = create_array_void(count);
+  for (int index = 0; index < count; index++)
+  {
+    array_void->array[index] = temp[index];
+  }
+  return array_void;
 }
 
 Object reduce_void(ArrayVoid_ptr src, Object init, ReducerVoid reducer)
@@ -51,7 +46,7 @@ Object reduce_void(ArrayVoid_ptr src, Object init, ReducerVoid reducer)
   Object context = init;
   for (int index = 0; index < src->length; index++)
   {
-    context = reducer(context, src->array[index]);
+    context = (*reducer)(context, src->array[index]);
   }
   return context;
 }
--- numbers.c
+++ numbers.c
@@ -28,35 +28,28 @@ void display_array(Array_ptr array)
 
 Object add_void(Object a, Object b)
 {
-  long int total = (long int)a + (long int)b;
-  return (Object)total;
+  Object result = malloc(sizeof(int));
+  *(int *)result = *(int *)a + *(int *)b;
+  return result;
 }
 
 Bool is_odd_void(Object number)
 {
-  return (int)number % 2 ? False : True;
+  return *(int *)number % 2 ? False : True;
 }
 
 Object square_void(Object number)
 {
-  long int result = (long int)number * (long int)number;
-  return (Object)result;
+  Object result = malloc(sizeof(int));
+  *(int *)result = *(int *)number * *(int *)number;
+  return result;
 }
 
 void display_number_array(ArrayVoid_ptr array)
 {
   for (int index = 0; index < array->length; index++)
   {
-    printf("%d ", (int)array->array[index]);
-  }
-  printf("\n");
-}
-
-void display_character_array(ArrayVoid_ptr array)
-{
-  for (int index = 0; index < array->length; index++)
-  {
-    printf("%c ", (char)array->array[index]);
+    printf("%d ", *(int *)array->array[index]);
   }
   printf("\n");
 }
@@ -78,21 +71,33 @@ int main(void)
 
   printf("---void array's---\n");
 
-  ArrayVoid_ptr array_void = create_array_void(length);
-  array_void->array[0] = (Object)(1);
-  array_void->array[1] = (Object)(2);
-  array_void->array[2] = (Object)(3);
-  array_void->array[3] = (Object)(4);
-  array_void->array[4] = (Object)(5);
+  ArrayVoid_ptr array_void = create_array_void(5);
+
+  int *numbers = malloc(sizeof(int) * array_void->length);
+  *(numbers + 0) = 1;
+  *(numbers + 1) = 2;
+  *(numbers + 2) = 3;
+  *(numbers + 3) = 4;
+  *(numbers + 4) = 5;
+  array_void->array[0] = numbers + 0;
+  array_void->array[1] = numbers + 1;
+  array_void->array[2] = numbers + 2;
+  array_void->array[3] = numbers + 3;
+  array_void->array[4] = numbers + 4;
+
   ArrayVoid_ptr empty_array_void = create_array_void(0);
 
-  display_number_array(array_void);
-  display_number_array(map_void(array_void, square_void));
-  display_number_array(map_void(empty_array_void, square_void));
-  display_number_array(filter_void(array_void, is_odd_void));
-  display_number_array(filter_void(empty_array_void, is_odd_void));
-  printf("%d\n", (int)reduce_void(array_void, 0, add_void));
-  printf("%d\n", (int)reduce_void(empty_array_void, 0, add_void));
+  display_number_array(array_void);                                 //displaying the array_void_elements
+  display_number_array(map_void(array_void, square_void));          //testing map with some elements in array_void
+  display_number_array(map_void(empty_array_void, square_void));    //testing map with empty array_void
+  display_number_array(filter_void(array_void, is_odd_void));       //testing filter with some elements in array_void
+  display_number_array(filter_void(empty_array_void, is_odd_void)); //testing filter with empty array_void
+
+  int *context = malloc(sizeof(int));
+  *context = 0;
+
+  printf("%d\n", *(int *)reduce_void(array_void, context, add_void));       //testing reduce with some elements in array_void
+  printf("%d\n", *(int *)reduce_void(empty_array_void, context, add_void)); //testing reduce with empty array_void
 
   return 0;
 }
